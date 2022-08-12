03330b0ac49e55460cb441927e930c1f735342f9
Kiran Anand <liteninkiran@hotmail.com>
2022-08-12 20:09:25 +0100

    Fix mistakes

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -8,7 +8,7 @@
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="{{ route('home.index') }}" class="nav-link px-2 text-white">Home</a></li>
                 @auth
-                    @role('Admin')
+                    @role('admin')
                         <li><a href="{{ route('users.index') }}" class="nav-link px-2 text-white">Users</a></li>
                         <li><a href="{{ route('roles.index') }}" class="nav-link px-2 text-white">Roles</a></li>
                     @endrole
@@ -16,12 +16,10 @@
                 @endauth
             </ul>
 
-            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-                <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-            </form>
-
             @auth
-                {{ auth()->user()->name }}&nbsp;
+                <div class="me-3">
+                    {{ auth()->user()->name }}
+                </div>
                 <div class="text-end">
                     <a href="{{ route('logout.perform') }}" class="btn btn-outline-light me-2">Logout</a>
                 </div>
